4700c967a2de218639f8500fba63a063f2b40896
David-solly <David.solomon_@outlook.com>
2020-03-04 11:32:49 +0000

    models for snatchbot buttons

--- lib/models/api_specific_models/snatchbot_chat_button_popup_model.dart
+++ lib/models/api_specific_models/snatchbot_chat_button_popup_model.dart
@@ -1,7 +1,18 @@
-class SnatchbotChatButtonPopupModel {
+class SnatchbotChatCardModel {
   final String type;
   final value;
   final List buttons;
+  final List galery;
 
-  SnatchbotChatButtonPopupModel({this.type, this.value, this.buttons});
+  SnatchbotChatCardModel({this.galery, this.type, this.value, this.buttons});
+}
+
+class SnatchbotButtonPopUp {}
+
+class SnatchbotButton {
+  final String title;
+  final type;
+  final url;
+
+  SnatchbotButton({this.title, this.type, this.url});
 }
